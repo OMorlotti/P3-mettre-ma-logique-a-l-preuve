b5e888160f7406bf894836da4161e4584b8178f3
Olivier Morlotti <o.morlotti@gmail.com>
2020-03-11 11:56:01 +0100

    update

--- src/main/java/xyz/morlotti/escapegame/Log.java
+++ src/main/java/xyz/morlotti/escapegame/Log.java
@@ -3,28 +3,15 @@ package xyz.morlotti.escapegame;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.Configurator;
 
 public class Log
 {
-	static // appliqué une seule fois : lorsque la classe est chargée en mémoire
-	{
-		try(LoggerContext context = (LoggerContext) LogManager.getContext(false))
-		{
-			context.setConfigLocation(Log.class.getResource("/log4j.xml").toURI());
-		}
-		catch(Exception e)
-		{
-			System.out.println(e.getMessage());
-		}
-	}
-
-	public static Logger getLogger(String name, boolean isDevelopperMode) // récupère le logger dont le nom est "name"
+	public static Logger getLogger(String name, boolean isDeveloperMode) // récupère le logger dont le nom est "name"
 	{
 		Logger logger = LogManager.getLogger(name);
 
-		if(isDevelopperMode)
+		if(isDeveloperMode)
 		{
 			Configurator.setLevel(name, Level.INFO);
 		}
